d1ccf7355063af2cc8c52d2f794c4de17a98e1f5
Zacgamingpro1234 <zac.vella.mt@gmail.com>
2026-09-24 22:17:33 +0200

    Set-up naming

--- src/main/java/com/github/zacgamingpro1234/titanium/Titanium.java
+++ src/main/java/com/github/zacgamingpro1234/titanium/Titanium.java
@@ -1,16 +1,16 @@
-package com.example.example_mod;
+package com.github.zacgamingpro1234.titanium;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.ornithemc.osl.entrypoints.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class Titanium implements ModInitializer {
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger("Example Mod");
+	public static final Logger LOGGER = LogManager.getLogger("Titanium");
 
 	@Override
 	public void init() {
--- src/main/java/com/github/zacgamingpro1234/titanium/mixin/TitleScreenMixin.java
+++ src/main/java/com/github/zacgamingpro1234/titanium/mixin/TitleScreenMixin.java
@@ -1,19 +1,19 @@
-package com.example.example_mod.mixin;
+package com.github.zacgamingpro1234.titanium.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.example.example_mod.ExampleMod;
+import com.github.zacgamingpro1234.titanium.Titanium;
 
-import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.gui.screen.menu.TitleScreen;
 
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin {
 
 	@Inject(method = "init", at = @At("TAIL"))
 	private void exampleMod$onInit(CallbackInfo ci) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		Titanium.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
