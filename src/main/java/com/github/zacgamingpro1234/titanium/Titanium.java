293696743a904019a849fe8204d32b35694d1007
Zacgamingpro1234 <zac.vella.mt@gmail.com>
2026-09-25 15:58:28 +0200

    Make priority changing work

--- src/main/java/com/github/zacgamingpro1234/titanium/Titanium.java
+++ src/main/java/com/github/zacgamingpro1234/titanium/Titanium.java
@@ -1,8 +1,13 @@
 package com.github.zacgamingpro1234.titanium;
 
+import org.apache.commons.lang3.SystemUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import net.ornithemc.osl.entrypoints.api.ModInitializer;
+import org.polyfrost.oneconfig.api.notifications.v1.Notification;
+import org.polyfrost.oneconfig.api.notifications.v1.NotificationsManager;
+
+import java.io.IOException;
 
 public class Titanium implements ModInitializer {
 	public static final Logger LOGGER = LogManager.getLogger("Titanium");
@@ -12,5 +17,18 @@ public class Titanium implements ModInitializer {
 	public void init() {
 		LOGGER.info("Initializing Titanium!");
 		CONFIG.preload();
+		if (SystemUtils.IS_OS_WINDOWS) {
+			try {
+				String prio = TitaniumConfig.CPUprio;
+				long pid = ProcessHandle.current().pid();
+				ProcessBuilder pb = new ProcessBuilder("powershell.exe", "-NoProfile", "-Command",
+					"(Get-Process -Id "+pid+").PriorityClass = '"+prio+"'");
+				pb.start().waitFor();
+				LOGGER.info("Process Priority {} Applied to {}!", prio, pid);
+
+			} catch (IOException | InterruptedException e) {
+				LOGGER.error("Error while initializing Titanium!", e);
+			}
+		}
 	}
 }
--- src/main/java/com/github/zacgamingpro1234/titanium/TitaniumConfig.java
+++ src/main/java/com/github/zacgamingpro1234/titanium/TitaniumConfig.java
@@ -1,12 +1,16 @@
 package com.github.zacgamingpro1234.titanium;
 import org.polyfrost.oneconfig.api.config.v1.Config;
 import org.polyfrost.oneconfig.api.config.v1.annotations.Dropdown;
+import org.polyfrost.oneconfig.api.config.v1.annotations.Info;
 
 public final class TitaniumConfig extends Config {
 	public static final TitaniumConfig INSTANCE = new TitaniumConfig();
 
-	@Dropdown(title = "CPU Priority", options = {"Realtime", "High", "Above normal", "Normal", "Below normal", "Idle"})
+	@Dropdown(title = "CPU Priority", options = {"Realtime", "High", "AboveNormal", "Normal", "BelowNormal", "Idle"})
 	public static String CPUprio = "Above normal";
+
+	@Info(title = "Realtime can only be applied when Minecraft is launched as Admin")
+	private static boolean ign;
 
 	private TitaniumConfig() {
 		super("titaniumOrinthe.json", "Titanium", Category.PERFORMANCE);
